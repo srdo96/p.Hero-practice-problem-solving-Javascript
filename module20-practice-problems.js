f4992335e2436826c15bcbd742f796661c3ce4ca
Sakib Rahman <seu.sakib@gmail.com>
2022-01-31 23:28:32 +0600

    add task:2 find small number

--- module20-practice-problems.js
+++ module20-practice-problems.js
@@ -13,3 +13,18 @@ function getSmallNum(numList) {
 let list = [11, 13, 19, 22, 5, 1];
 let smallNum = getSmallNum(list);
 console.log("The Small No: ", smallNum);
+
+// task:2 find small number
+let num1 = 3,
+  num2 = 9,
+  num3 = 50;
+
+if (num1 < num2) {
+  if (num1 < num3) {
+    console.log("Small Num: ", num1);
+  } else console.log("Small Num: ", num3);
+} else {
+  if (num2 < num3) {
+    console.log("Small Num: ", num2);
+  } else console.log("Small Num: ", num3);
+}
