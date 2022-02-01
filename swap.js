737168419ec1dbf0198b4b61140e5c4f1f0e0902
Sakib Rahman <seu.sakib@gmail.com>
2022-02-01 11:47:45 +0600

    add destructuring method

--- swap.js
+++ swap.js
@@ -1,7 +1,13 @@
+// method 1
 let a = 4;
 let b = 9;
 a = a + b;
 b = a - b;
 a = a - b;
+console.log("a: ", a);
+console.log("b: ", b);
+
+// method 2 : destructuring method
+[a, b] = [b, a];
 console.log("a: ", a);
 console.log("b: ", b);
